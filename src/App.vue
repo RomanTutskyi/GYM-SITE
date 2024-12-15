d827db2c4f267f272a515294c74c96a9d01e18d3
TutskyiRoma <tuskyir@gmail.com>
2024-12-16 00:25:54 +0200

    1

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {Header},
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <Header />
+
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+}
+
+.container{
+  width: 100%;
+  height: 100vh;
+  background-image: url("../public/pexels-pixabay-416778.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  font-family: 'Oswald', sans-serif;
+  position: absolute;
+  font-size: 1em;
+  color: white;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <div class="header">
+    <div class="logo">
+      <div class="text">SPORT PAGE</div>
+      <div class="text">Your Tagline here</div>
+    </div>
+    <div class="info">
+      <ul>
+        <li><a>Home</a></li>
+        <li><a>Tarrifs</a></li>
+        <li><a>Partners</a></li>
+        <li><a>Contact us</a></li>
+        <li><a href="/"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+        <li><a><button>Sign in</button></a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  width: 90%;
+  margin: 10px auto;
+  position: relative;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    font-size: 2em;
+
+    .text{
+      font-weight: bold;
+
+      &:first-of-type{
+        color: #c506c5;
+      }
+    }
+  }
+
+
+  .info ul {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    list-style: none;
+
+    li {
+      margin-left: 25px;
+      font-size: 1.5em;
+    }
+  }
+}
+</style>
